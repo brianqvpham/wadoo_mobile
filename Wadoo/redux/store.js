cfd445f236c365e386ebd91e733527e1309f38f4
Brian <brianqvpham@hotmail.com>
2016-10-23 01:18:12 -0500

    Added queries to obtain event information. Events must be displayed on event page.

--- Wadoo/redux/store.js
+++ Wadoo/redux/store.js
@@ -2,6 +2,15 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 
 import {reducer} from './reducer'
 import {setEvents, setUser} from './actions'
+import * as WindowsAzure from 'azure-mobile-apps-client';
+import {Alert} from 'react-native';
+
+// Create client connection
+var client = WindowsAzure.MobileServiceClient("wadoo-mobile.azurewebsites.net");
+
+// Create reference to tables
+var event_table = client.getTable("Event");
+var user_event_table = client.getTable("User_Event");
 
 const store = createStore(reducer);
 
@@ -15,4 +24,32 @@ export function SetEvents(events){
 
 export function SetUser(user){
 	store.dispatch(setUser(user));
+}
+
+// if SQL request fails
+function failure(error) {
+  console.log('Error loading event data: ');
+  Alert.alert('Unable to get events.');
+}
+
+// Stores the events
+function storeEvents(results) {
+	SetEvents(results);
+}
+
+// Uses event ids to query for events
+fuction getEventIDS(results) {
+  event_table
+		.orderBy('date')
+    .read()
+    .then(storeEvents, failure);
+}
+
+
+function getEvents() {
+  // Query for event ids tied to user
+  user_event_table.select('event_id')
+    .where({user_id : GetState().user})
+    .read()
+    .then(getEventIDS, failure);
 }
---
+++ Wadoo/scenes/events.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import {getEvents} from '../redux/reducer';
+
+export const EVENT = "EVENT"
+
+export function renderEvent(navigator){
+	console.log("RenderEvent")
+  getEvents();
+	return(
+		<View style={styles.container}>
+			<Text>This is the events page</Text>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex:1,
+		alignItems: 'center',
+		justifyContent:'center',
+		backgroundColor:'#cc6600',
+	},
+	textStyle: {
+		color:"#cc6600"
+	}
+})
