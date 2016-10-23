6b31b166adf62ce1564faf7a142a79ec93847f62
Jaime Munoz <jaimemunz@gmail.com>
2016-10-23 06:57:14 -0500

    Made the swiping cards

--- Wadoo/scenes/home_activity.js
+++ Wadoo/scenes/home_activity.js
@@ -1,3 +1,4 @@
+'use stright';
 import React, { Component } from 'react';
 import {
     StyleSheet,
@@ -18,39 +19,69 @@ import {
 
 import Button from 'react-native-button'
 
-const width = Dimensions.get('window').width
+import SwipeCards from 'react-native-swipe-cards';
+
 const theme = getTheme();
 
 export const HOME = "HOME"
 
+var {
+    width,
+    height
+} = Dimensions.get('window');
 
-class ListViewEvents extends Component {
-    // Initialize the data
-    constructor(props) {
-        super(props);
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.state = {
-            dataSource: ds.cloneWithRows([
-                'Event1', 'Event2', 'Event3', 'Event4', 'Event5'
-            ])
-        };
-    }render() {
+let Card = React.createClass({
+    render() {
         return (
-                <View style={{paddingTop: 22}}>
-                <ListView
-                    dataSource= {this.state.dataSource}
-                    renderRow= {(rowData) => <Text>{rowData}</Text>}
-                />
+                <View style= {[styles.card, {backgroundColor: this.props.backgroundColor}]}>
+                <Text>{this.props.text}</Text>
                 </View>
-        );
+        )
     }
-}
+})
 
+const Cards = [
+    {text: 'Event One', backgroundColor: 'red'},
+    {text: 'Event Two', backgroundColor: 'purple'},
+    {text: 'Event Three', backgroundColor: 'green'},
+    {text: 'Event Four', backgroundColor: 'blue'},
+    {text: 'Event Five', backgroundColor: 'cyan'},
+    {text: 'Event Six', backgroundColor: 'orange'},
+]
+
+var CardsSwipe =  React.createClass({
+    getInitialState(){
+        return {
+            cards: Cards
+        }
+    },
+    handleYup (card) {
+        console.log('Yup for ${card.text}')
+    },
+    handleNope (card) {
+        console.log('Nope for ${card.text}')
+    },
+    render() {
+        return (
+                <SwipeCards
+            cards = {this.state.cards}
+
+            renderCard={(cardData) => <Card {...cardData} />}
+            renderNoMoreCards={() => <NoMoreCards />}
+
+            handleYup={this.handleYup}
+            handleNope={this.handleNope}
+                />
+        )
+    }
+})
 
 export function renderHome(navigator) {
     console.log("RenderHomePage")
     var base64Icon = 'http://www.getmdl.io/assets/demos/welcome_card.jpg';
     return (
+        <CardsSwipe/>
+            /*
             <View style={theme.cardStyle}>
             <Image source={{uri : base64Icon}} style={theme.cardImageStyle} />
             <Text style={theme.cardTitleStyle}>A Cool Event</Text>
@@ -70,9 +101,9 @@ export function renderHome(navigator) {
             Not Interested
             </Button>
             </View>
-
         </View>
-    );
+            */
+);
 }
 
 function _handlePress(event){
@@ -80,30 +111,60 @@ function _handlePress(event){
 }
 
 
+class ListViewEvents extends Component {
+    // Initialize the data
+    constructor(props) {
+        super(props);
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {
+            dataSource: ds.cloneWithRows([
+                'Event1', 'Event2', 'Event3', 'Event4', 'Event5'
+            ])                                     
+        };
+    }render() {
+        return (
+                <View style={{paddingTop: 22}}>
+                <ListView
+                    dataSource= {this.state.dataSource}
+                    renderRow= {(rowData) => <Text>{rowData}</Text>}
+                />
+                </View>
+        );
+    }
+}
+
+
+
 var styles = StyleSheet.create({
-  toolbar: {
-    backgroundColor: 'rgba(245,252,255,.98)',
-    height: 56,
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-  },
   button: {
       borderWidth: 1,
       borderColor: 'blue',
       width: (width/2)
   },
-
   list: {
     backgroundColor: '#F5FCFF',
     paddingTop: 64,
   },
   container: {
     flex: 1,
-    alignItems: 'stretch',
+    flexDirection: 'column'
   },
-  welcome: {
+    outerScroll:{
+        flex: 1,
+        flexDirection: 'column'
+    },
+    row: {
+        flex: 1
+    },
+    card: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 300,
+        height: 300
+    },
+
+    welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
